5b454e4b49d5473ee050638704b88974cb2d31f3
terrarishabh <rishabhsingh@terra-charge.co.in>
2024-07-15 14:07:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 // Create an instance of Express
 const app = express();
-const port = 3000; // Choose any port number you like
+const port = process.env.PORT || 4000; // Choose any port number you like
 
 // Define a route handler for the root path
 app.get('/1', (req, res) => {
