b4be6d97caa60563a19a8ae58f174e211708bddd
terrarishabh <rishabhsingh@terra-charge.co.in>
2024-07-15 15:23:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,21 @@
-// Import required modules
 const express = require('express');
 
-// Create an instance of Express
 const app = express();
 const port = process.env.PORT || 4000; // Choose any port number you like
 
-// Define a route handler for the root path
 app.get('/1', (req, res) => {
-    // Create a JSON response
     const response = {
         message: '1000'
     };
     
-    // Send the JSON response
     res.json(response);
 });
 
 app.get('/2', (req, res) => {
-    // Create a JSON response
     const response = {
         message: '2000'
     };
     
-    // Send the JSON response
     res.json(response);
 });
 
